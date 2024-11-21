c4e81702ef674886fe457e426f669d8a0bcf7f05
tanvir-alam-sk <tanviralamsk47@gmail.com>
2024-11-21 12:55:53 +0600

    initial setup

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask;
+from flask import jsonify;
+from flask import request;
+
+app=Flask(__name__);
+
+@app.route("/",methods=["GET"])
+def home1():
+    print("hallo world2");
+    return "Hallo World114"
+
+@app.route("/user",methods=["POST"])
+def post_user():
+    data=request.get_json()
+    print(data);
+
+
+
+
+if __name__== "__main__":
+    app.run(debug=True)
