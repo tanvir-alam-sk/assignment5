1f52f4991fff319422008b07f9c0b73c4717462e
tanvir-alam-sk <tanviralamsk47@gmail.com>
2024-11-21 13:04:10 +0600

    initial

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ def home1():
     print("hallo world2");
     return "Hallo World114"
 
+
+@app.route("/user",methods=["get"])
+def get_user():
+    return "get user"
+
+
+
 @app.route("/user",methods=["POST"])
 def post_user():
     data=request.get_json()
